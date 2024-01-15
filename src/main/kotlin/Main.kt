42e9884d36e0a117230b3dd60fc0684c708191d2
Guilherme Brisson <54915600+guiBrisson@users.noreply.github.com>
2024-01-15 20:06:49 -0300

    Base IconButton component

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,4 @@
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -9,7 +7,6 @@ import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -17,6 +14,7 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import ollama.presentation.chat.ChatScreen
 import ollama.presentation.chat_history.ChatHistoryScreen
+import ui.components.IconButton
 import ui.theme.AlpacaTheme
 
 @Composable
@@ -32,21 +30,18 @@ fun App() {
                 }
 
                 // expand/collapse side menu button
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.TopStart)
-                        .padding(4.dp)
-                        .clip(RoundedCornerShape(6.dp))
-                        .clickable { historyChatExpanded = !historyChatExpanded },
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Icon(
-                        modifier = Modifier.padding(4.dp).size(24.dp),
-                        imageVector = Icons.Rounded.Menu,
-                        contentDescription = "Expand/collapse side menu",
-                        tint = Color(0xff888889),
-                    )
-                }
+                IconButton(
+                    modifier = Modifier.align(Alignment.TopStart),
+                    onClick = { historyChatExpanded = !historyChatExpanded },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.padding(4.dp).size(24.dp),
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "Expand/collapse side menu",
+                            tint = Color(0xff888889),
+                        )
+                    },
+                )
             }
         }
     }
